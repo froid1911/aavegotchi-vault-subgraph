96be7071a3da47927854b06637e66e12a63ef68a
Frank <frank.pfeift@protonmail.com>
2022-02-02 09:50:50 +0100

    remove aavegotchi from store after withdraw

--- src/helper.ts
+++ src/helper.ts
@@ -1,9 +1,18 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { Aavegotchi, Vault } from "../generated/schema";
 
-export function getOrCreateVault(address: Address) {
-
+export function getOrCreateVault(address: Address): Vault {
+    let vault = Vault.load(address.toHexString());
+    if(!vault) {
+        vault = new Vault(address.toHexString());
+    }
+    return vault;
 }
 
-export function getOrCreateAavegotchi(id: BigInt) {
-
+export function getOrCreateAavegotchi(id: BigInt): Aavegotchi {
+    let gotchi = Aavegotchi.load(id.toString());
+    if(!gotchi) {
+        gotchi = new Aavegotchi(id.toString());
+    }
+    return gotchi;
 }
